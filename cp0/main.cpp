c6af5fb9fd402a26dfccf483922f1ea981fcfeb9
bowen163 <763893371@qq.com>
2019-06-20 11:17:12 +0800

    liangbiao

--- cp0/main.cpp
+++ cp0/main.cpp
@@ -7,9 +7,33 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include<fstream>
+using namespace std;
+int main()
+{
+    int a,b;
+    ifstream putin1("/Users/s20181102936/Desktop/student.txt");
+    ifstream putin2("/Users/s20181102936/Desktop/caipan.txt");
+    ofstream putout ("/Users/s20181102936/Desktop/score1.txt ");
+    if(putin1.is_open())
+    {
+        cout<<"file ok"<<endl;
+        putin1>>a;
+        cout<<a<<"  "<<endl;
+        putin1.close();
+    }
+    if(putin2.is_open())
+    {
+        cout<<"file ok"<<endl;
+        putin2>>b;
+        cout<<b<<endl;
+        putin2.close();
+    }
+    if(putout.is_open())
+    {
+        putout<<"c="<<a+b<<endl;
+        cout<<"c="<<a+b<<endl;
+        putout.close();
+    }
     return 0;
 }
